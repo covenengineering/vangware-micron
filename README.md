c193286b96e270cf5cf4367d19e5892946320f1f
Lucas Matías Ciruzzi <lucasmciruzzi@live.com>
2016-05-14 16:59:11 -0300

    v1.9.0 - Added mrx

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![[Vangware logo]](http://vangware.github.io/micron/assets/img/logo.min.svg)](https://vangware.com)
 
-micron v1.8.5 [![[Build Status]](https://img.shields.io/travis/vangware/micron.svg?style=flat-square)](https://travis-ci.org/vangware/micron)
+micron v1.9.0 [![[Build Status]](https://img.shields.io/travis/vangware/micron.svg?style=flat-square)](https://travis-ci.org/vangware/micron)
 ----
 
 Standalone helper arrow functions (ES6) in 1 line. Useful for code injection, bookmarklets, debugging, etc.
@@ -12,6 +12,7 @@ cks - Get the current document cookies in object form.
 css - Set several styles of an element or a group of elements.
 del - Remove an element or a group of elements from the DOM.
 get - Alias for querySelectorAll, but returning an array instead of a nodeList.
+mrx - Takes a string and an object and makes a regex map replace.
 rnd - Random string generator (up to 16 characters).
 url - Parse an object into a simple string in URL format for XHR.
 xhr - Alias for new XMLHttpRequest, with GET method by default.
--- micron.js
+++ micron.js
@@ -1,6 +1,6 @@
 /** 
  * @file micron.js - Standalone helper arrow functions (ES6) in 1 line.
- * @version 1.8.5
+ * @version 1.9.0
  * @author Vangware - https://vangware.com
  */
 
@@ -15,7 +15,7 @@
  * @param {Function} c - Callback Function.
  * @returns {Element[]} E - DOM Element or Array of DOM Elements with event.
  */
-let ael=(E,V)=>(E.length?E:[E]).map(e=>Object.keys(V).map(v=>(e.addEventListener(v,V[v]),e))[0]);
+const ael=(E,V)=>(E.length?E:[E]).map(e=>Object.keys(V).map(v=>(e.addEventListener(v,V[v]),e))[0]);
 
 /**
  * Set several attributes of an element or a group of elements.
@@ -24,14 +24,14 @@ let ael=(E,V)=>(E.length?E:[E]).map(e=>Object.keys(V).map(v=>(e.addEventListener
  * @param {Object} A - List of attributes in Object format.
  * @returns {Element[]} E - DOM Element or Array of DOM Elements with new attributes.
  */
-let atr=(E,A)=>(E.length?E:[E]).map(e=>Object.keys(A).map(a=>(e.setAttribute(a,A[a]),e))[0]);
+const atr=(E,A)=>(E.length?E:[E]).map(e=>Object.keys(A).map(a=>(e.setAttribute(a,A[a]),e))[0]);
 
 /**
  * Get the current document cookies in object form
  *
  * @returns {Object} o - The document cookies object
  */
-let cks=()=>JSON.parse("{"+document.cookie.split("; ").map(e=>e.replace(/"/g,'\\"').replace("=",'":"')).join('","')+"}");
+const cks=()=>JSON.parse("{"+document.cookie.split("; ").map(e=>e.replace(/"/g,'\\"').replace("=",'":"')).join('","')+"}");
 
 /**
  * Set several styles of an element or a group of elements.
@@ -40,7 +40,7 @@ let cks=()=>JSON.parse("{"+document.cookie.split("; ").map(e=>e.replace(/"/g,'\\
  * @param {Object} S - List of styles in Object format.
  * @returns {Element[]} E - DOM Element or Array of DOM Elements with new styles.
  */
-let css=(E,S)=>(E.length?E:[E]).map(e=>Object.keys(S).map(s=>(e.style[s]=S[s],e))[0]);
+const css=(E,S)=>(E.length?E:[E]).map(e=>Object.keys(S).map(s=>(e.style[s]=S[s],e))[0]);
 
 /**
  * Remove an element or a group of elements from the DOM.
@@ -48,7 +48,7 @@ let css=(E,S)=>(E.length?E:[E]).map(e=>Object.keys(S).map(s=>(e.style[s]=S[s],e)
  * @param {(Element|Element[])} E - DOM Element or Array of DOM Elements to remove from DOM
  * @returns {Element[]} E - DOM Element or Array of DOM Elements removed from DOM
  */
-let del=E=>(E.length?E:[E]).map(e=>e.parentElement.removeChild(e));
+const del=E=>(E.length?E:[E]).map(e=>e.parentElement.removeChild(e));
 
 /**
  * Alias for querySelectorAll, but returning an array instead of a nodeList.
@@ -57,7 +57,15 @@ let del=E=>(E.length?E:[E]).map(e=>e.parentElement.removeChild(e));
  * @param {Element} [e=document] - Base element.
  * @returns {Element[]} - Array of elements.
  */
-let get=(q,e)=>Array.from((e||document).querySelectorAll(q));
+const get=(q,e)=>Array.from((e||document).querySelectorAll(q));
+
+/**
+ * Takes a string and an object and makes a regex map replace
+ * @param  {string} s - String
+ * @param  {Object} m - Map with format { "strin to be replaced": "replacing string" }
+ * @return {string} s - String with replaced elements from map
+ */
+const mrx=(s,m)=>s.replace(new RegExp(Object.keys(m).map(c=>c.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g,"\\$&")).join("|"),"g"),c=>m[c]);
 
 /**
  * Random string generator (up to 16 characters).
@@ -65,7 +73,7 @@ let get=(q,e)=>Array.from((e||document).querySelectorAll(q));
  *
  * @param {number} l - Length of the random string
  */
-let rnd=l=>(Math.random()+1).toString(36).substr(2,l);
+const rnd=l=>(Math.random()+1).toString(36).substr(2,l);
 
 /**
  * Parse an object into a simple string in URL format for XHR.
@@ -73,7 +81,7 @@ let rnd=l=>(Math.random()+1).toString(36).substr(2,l);
  * @param {Object} o - List of input data for ajax in Object format.
  * @returns {string} - URL formated string.
  */
-let url=o=>Object.keys(o).map(p=>p+"="+encodeURIComponent(o[p])).join("&");
+const url=o=>Object.keys(o).map(p=>p+"="+encodeURIComponent(o[p])).join("&");
 
 /**
  * Alias for new XMLHttpRequest, with GET method by default.
@@ -82,4 +90,4 @@ let url=o=>Object.keys(o).map(p=>p+"="+encodeURIComponent(o[p])).join("&");
  * @param {string} [m=GET] - Method.
  * @returns {XMLHttpRequest} x - The opened XML HTTP Request.
  */
-let xhr=(u,m)=>{let x=new XMLHttpRequest;return x.open(m||"GET",u),x};
+const xhr=(u,m)=>{let x=new XMLHttpRequest;return x.open(m||"GET",u),x};
