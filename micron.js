a0a10a6ba81b4c80a7e1a3deab2f0ec0d6209f7f
Lou Cyx <git@lou.cx>
2016-10-26 10:26:46 -0300

    v2.2.0 - Improved url function.

--- micron.js
+++ micron.js
@@ -1,6 +1,6 @@
 /** 
  * @file Standalone helper arrow functions (ES6) in 1 line.
- * @version 2.1.0
+ * @version 2.2.0
  * @author Vangware - https://vangware.com
  */
 
@@ -125,11 +125,12 @@ const rnd=l=>(Math.random()+1).toString(36).substr(2,l);
  *
  * @example
  * url({ a: 1, b: 2, c: 3 }); // Returns "a=1&b=2&c=3"
+ * url({ a: [1, 2, 3], b: { c: 1, d: 2 } }) // Returns "a[0]=1&a[1]=2&a[2]=3&b[c]=1&b[d]=2"
  *
  * @param {Object} o List of input data for ajax in Object format.
  * @returns {string} URL formated string.
  */
-const url=o=>Object.keys(o).map(p=>Array.isArray(o[p])?o[p].map(r=>p+"[]="+r).join("&"):(typeof o[p]==="object"&&o[p]!==null)?url(Object.keys(o[p]).reduce((r,q)=>Object.assign(r,{[p+`[${q}]`]:o[p][q]}),{})):p+"="+encodeURIComponent(o[p])).join("&");
+const url=o=>Object.keys(o).map(p=>Array.isArray(o[p])?o[p].map((r,q)=>url({[p+`[${q}]`]:r})).join("&"):(typeof o[p]==="object"&&o[p]!==null)?url(Object.keys(o[p]).reduce((r,q)=>Object.assign(r,{[p+`[${q}]`]:o[p][q]}),{})):p+"="+encodeURIComponent(o[p])).join("&");
 
 /**
  * Alias for new XMLHttpRequest, with GET method by default.
